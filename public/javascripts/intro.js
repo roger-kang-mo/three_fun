525cd67ef879926375f8592a23a1e79978d32205
Roger <roger.kang.mo@gmail.com>
2016-06-27 09:59:20 -0700

    cubes

--- public/javascripts/intro.js
+++ public/javascripts/intro.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   var renderer = new THREE.WebGLRenderer();
 
   var geometry = new THREE.BoxGeometry(1,1,1);
-  var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+  var material = new THREE.MeshBasicMaterial({color: 0xcccccc});
   var cube = new THREE.Mesh(geometry, material);
 
   renderer.setSize( window.innerWidth, window.innerHeight );
@@ -18,12 +18,15 @@ $(document).ready(function(){
   var render = function() {
     requestAnimationFrame(render);
 
-    cube.rotation.x += 0.001;
-    cube.rotation.y += 0.01`;
+    animationChanges();
 
     renderer.render(scene, camera);
   };
 
-  render()
+  var animationChanges = function() {
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.01;
+  };
 
+  render();
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'three' });
 });
 
+router.get('/cubes', function(req, res) {
+  res.render('cubes', { title: 'three' });
+});
 
 module.exports = router;
